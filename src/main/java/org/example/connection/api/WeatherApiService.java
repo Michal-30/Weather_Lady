1e3aadaabc9ad0d13685364e4c8e692f59aab9d0
Stefan <stefan.pocsik@gmail.com>
2024-11-02 12:24:45 +0100

    create WeatherApiService

---
+++ src/main/java/org/example/connection/api/WeatherApiService.java
@@ -0,0 +1,43 @@
+package org.example.connection.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WeatherApiService {
+    private HttpClient httpClient;
+    private String apiUrl;
+
+    public WeatherApiService() {
+        this.apiUrl = "https://api.open-meteo.com/v1/forecast";
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    public String getWeatherByCord(double lat, double lan) {
+        StringBuilder url = new StringBuilder(this.apiUrl).append("?latitude=")
+                .append(lat)
+                .append("&longitude=")
+                .append(lan)
+                .append("&hourly=temperature_2m&timezone=Europe%2FBerlin");
+
+        try {
+            URI uri = new URI(url.toString());
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "[{}]";
+        }
+    }
+}
