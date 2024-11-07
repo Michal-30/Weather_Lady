31556c7ef9084ec8e9af0a1c7e0c57a37db506d7
I have an idea <49106207+christosste86@users.noreply.github.com>
2024-11-07 10:52:31 +0100

    nothing

--- src/main/java/org/example/connection/db/daos/GenericDao.java
+++ src/main/java/org/example/connection/db/daos/GenericDao.java
@@ -15,30 +15,30 @@ public class GenericDao <T, ID extends Serializable> {
         this.entityType = entityType;
     }
 
-    public void save(T entity){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+    public void save(T entity) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.save(entity);
             transaction.commit();
         }
     }
 
-    public T getById(ID id){
+    public T getById(ID id) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.get(entityType, id);
         }
     }
 
-    public void update(T entity){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+    public void update(T entity) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.update(entity);
             transaction.commit();
         }
     }
 
-    public void delete(T entity){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+    public void delete(T entity) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.delete(entity);
             transaction.commit();
